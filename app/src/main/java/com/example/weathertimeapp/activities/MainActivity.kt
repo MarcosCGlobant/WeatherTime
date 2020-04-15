55a90da129688e0f7cf6b67d697e2da624838bc3
Marcos Cifuentes <marcos.cifuentes@globant.com>
2020-04-15 13:04:06 -0300

    implemented MVP structure and auto-complete funcionality

---
+++ app/src/main/java/com/example/weathertimeapp/activities/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.weathertimeapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.example.weathertimeapp.R
+import com.example.weathertimeapp.mvp.contracts.WeatherTimeContracts
+import com.example.weathertimeapp.mvp.model.WeatherTimeModel
+import com.example.weathertimeapp.mvp.presenter.WeatherTimePresenter
+import com.example.weathertimeapp.mvp.view.WeatherTimeView
+import kotlinx.android.synthetic.main.activity_main.activity_main_edit_text_city
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var presenter: WeatherTimeContracts.Presenter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val jsonStringCities = applicationContext.assets.open(FILE_NAME).bufferedReader().use {
+            it.readText()}
+        presenter = WeatherTimePresenter(WeatherTimeModel(readCitiesJSONFile()), WeatherTimeView(this))
+        presenter.createAndSetCitiesList()
+    }
+
+    private fun readCitiesJSONFile(): String {
+        return applicationContext.assets.open(FILE_NAME).bufferedReader().use {
+            it.readText()
+        }
+    }
+
+    companion object {
+        private const val FILE_NAME = "city.list.json"
+    }
+}
---
+++ app/src/main/java/com/example/weathertimeapp/mvp/presenter/WeatherTimePresenter.kt
@@ -0,0 +1,18 @@
+package com.example.weathertimeapp.mvp.presenter
+
+import com.example.weathertimeapp.mvp.contracts.WeatherTimeContracts
+import com.example.weathertimeapp.mvp.entities.City
+
+class WeatherTimePresenter(
+    private val model: WeatherTimeContracts.Model,
+    private val view: WeatherTimeContracts.View
+) : WeatherTimeContracts.Presenter {
+
+    override fun createAndSetCitiesList() {
+        view.setCitiesListAdapter(model.createListOfCities(cities))
+    }
+
+    companion object {
+        private var cities = mutableListOf<City>()
+    }
+}
