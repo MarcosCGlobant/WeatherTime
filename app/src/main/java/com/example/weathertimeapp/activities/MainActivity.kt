06f3fc3eeaa2947b08160259a0c367fe16d5cd73
Marcos Cifuentes <marcos.cifuentes@globant.com>
2020-04-15 15:06:29 -0300

    removed unnecessary code and unused imports

--- app/src/main/java/com/example/weathertimeapp/activities/MainActivity.kt
+++ app/src/main/java/com/example/weathertimeapp/activities/MainActivity.kt
@@ -2,13 +2,11 @@ package com.example.weathertimeapp.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
 import com.example.weathertimeapp.R
 import com.example.weathertimeapp.mvp.contracts.WeatherTimeContracts
 import com.example.weathertimeapp.mvp.model.WeatherTimeModel
 import com.example.weathertimeapp.mvp.presenter.WeatherTimePresenter
 import com.example.weathertimeapp.mvp.view.WeatherTimeView
-import kotlinx.android.synthetic.main.activity_main.activity_main_edit_text_city
 
 class MainActivity : AppCompatActivity() {
 
