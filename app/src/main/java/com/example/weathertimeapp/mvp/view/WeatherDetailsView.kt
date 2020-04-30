1f84ea42b182e0a8672f5c89eb2c3b4fa65f1ee4
Marcos Cifuentes <marcos.cifuentes@globant.com>
2020-04-30 16:38:51 -0300

    added weak reference for the dialog fragment MVP

--- app/src/main/java/com/example/weathertimeapp/mvp/view/WeatherDetailsView.kt
+++ app/src/main/java/com/example/weathertimeapp/mvp/view/WeatherDetailsView.kt
@@ -1,8 +1,10 @@
 package com.example.weathertimeapp.mvp.view
 
+import androidx.fragment.app.Fragment
 import com.example.weathertimeapp.activities.WeatherDetailsFragment
 import com.example.weathertimeapp.data.entities.Forecast
 import com.example.weathertimeapp.mvp.contracts.WeatherDetailsContract
+import com.example.weathertimeapp.mvp.view.base.FragmentView
 
-class WeatherDetailsView (fragment: WeatherDetailsFragment): WeatherDetailsContract.View{
+class WeatherDetailsView (fragment: Fragment): FragmentView(fragment), WeatherDetailsContract.View{
 }
---
+++ app/src/main/java/com/example/weathertimeapp/mvp/view/base/FragmentView.kt
@@ -0,0 +1,21 @@
+package com.example.weathertimeapp.mvp.view.base
+
+import android.app.FragmentManager
+import androidx.fragment.app.Fragment
+import java.lang.ref.WeakReference
+
+open class FragmentView(fragment: Fragment) {
+    private val fragmentRef: WeakReference<Fragment> = WeakReference(fragment)
+
+    val fragment: Fragment?
+        get() = fragmentRef.get()
+
+    val context: Fragment?
+        get() = fragment
+
+    val fragmentManager: androidx.fragment.app.FragmentManager?
+        get() {
+            val fragment = fragment
+            return fragment?.fragmentManager
+        }
+}
